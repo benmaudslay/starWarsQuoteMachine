a779f1cc2440174da9c898d259e0e199b8b5693c
Ben <ben@Bens-MacBook-Pro.local>
2020-05-27 16:38:03 +0100

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css"
+
+class App extends Component {
+  state = {
+    quote: "",
+  }
+
+  componentDidMount() {
+    this.handleFetchRequest()
+  }
+
+  handleFetchRequest = () => {
+    fetch("http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote")
+      .then((response) => response.json())
+      .then((data) => this.setState({ quote: data.starWarsQuote }))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <img
+          alt="Star Wars"
+          src="https://cdn.commonwealthclub.org/s3fs-public/styles/hero/public/2019-11/hero%20star%20wars.png?itok=waMDC-JL"
+        />
+        <h3>a random star wars quote</h3>
+        <hr />
+        <h1>{this.state.quote && this.state.quote.toLowerCase()}</h1>
+        <button onClick={this.handleFetchRequest}>new quote</button>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
